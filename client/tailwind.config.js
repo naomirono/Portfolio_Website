ca0c468b1d989dc242ac494ae4a8daa0b1b79643
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-12 21:09:13 +0300

    Add custom color to tailwind.config.js

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'custom-color': 'hsl(var(--hue-color), 69%, 61%)',
+      },
+    },
   },
   plugins: [],
 }
