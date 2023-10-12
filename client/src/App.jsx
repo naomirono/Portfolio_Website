27d6b0252b12c39f1562db544377c0fb9963d132
Naomi Cherotich <naomirono1454@gmail.com>
2023-10-12 21:50:44 +0300

    Import and render the components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useEffect } from 'react';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Skills from './components/Skills';
+import Qualification from './components/Qualification';
+import Services from './components/Service';
+import Portfolio from './components/Portfolio';
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  // useEffect replaces componentDidMount and componentDidUpdate
+  useEffect(() => {
+    const navToggle = document.getElementById('nav-toggle');
+    const navMenu = document.getElementById('nav-menu');
+
+    // MENU SHOW
+    if (navToggle) {
+      navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+      });
+    }
+
+    // MENU HIDDEN
+    const navClose = document.getElementById('nav-close');
+    if (navClose) {
+      navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+      });
+    }
+
+    // Clean up event listeners when component unmounts
+    return () => {
+      if (navToggle) navToggle.removeEventListener('click', () => {});
+      if (navClose) navClose.removeEventListener('click', () => {});
+      // ... Remove other event listeners here
+    };
+  }, []);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App main">
+      <Header />
+      <Home/>
+      <About/>
+      <Skills/>
+      <Qualification/>
+      <Services/>
+      <Portfolio/>
+    </div>
+  );
 }
 
-export default App
+export default App;
