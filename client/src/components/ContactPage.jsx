e9322dfc93c85ce08a229d3b3b022969cb59dcc7
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-03 17:53:43 +0300

    Import contact section

---
+++ client/src/components/ContactPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import ContactSection from './Contact';
+
+function ContactPage() {
+  return (
+    <div className="contact-page mt-24">
+      <ContactSection />
+    </div>
+  );
+}
+
+export default ContactPage;
