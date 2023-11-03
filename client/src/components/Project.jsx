dbc769e0387c153a0cfbf1b102b0c34fd28d6006
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-03 17:40:48 +0300

    Add links to lets get started button

--- client/src/components/Project.jsx
+++ client/src/components/Project.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { FaComment } from 'react-icons/fa';
 
 function Project() {
@@ -6,17 +7,17 @@ function Project() {
     <section className="project section">
       <div className="project_bg">
         <div className="project_container container grid">
-        <div className="project_data">
+          <div className="project_data">
             <h2 className="project_title">Embark on Your Next Project with Confidence</h2>
             <p className="project_description">
               Are you ready to turn your ideas into reality? With my expertise in web development, I'm equipped to
               bring your projects to life. From concept to execution, I'm dedicated to delivering high-quality,
               user-friendly solutions tailored to your unique needs.
             </p>
-            <a href="#contact" className="button button--flex button--white">
+            <Link to="/contact" className="button button--flex button--white">
               Let's Get Started
               <FaComment className="project_icon button_icon" />
-            </a>
+            </Link>
           </div>
           <img src="src/assets/IMG_5480.png" alt="" className="project_img" />
         </div>
