d0bb0d005c9dd6f325e4d51abc3871300672c782
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-03 17:31:17 +0300

    Add portfolio data and export it

---
+++ client/src/mock/portfolio.js
@@ -0,0 +1,26 @@
+export const portfolioItems = [
+    {
+      imgSrc: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/TreeHaven.png',
+      title: 'Tree Planting Website with Stripe and M-Pesa Integration',
+      description: 'Built a dynamic tree planting website utilizing React and Tailwind Css for the frontend, Node.js for the backend, and MongoDB for the database. Skillfully integrated both Stripe and M-Pesa payment gateways to streamline donation processing, showcasing expertise in front-end development, payment integration, and API utilization.',
+      demoLink: 'https://treehaven-neyo.web.app',
+    },
+    {
+      imgSrc: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/Atesta.png',
+      title: 'Construction Company Website',
+      description: 'Developed and designed a static website for a construction company as a personal project, using HTML, CSS, and JavaScript. Designed a visually appealing and a user-friendly website that showcases the companys services and projects. Implemented responsive design techniques to ensure the website looked great on desktop and mobile devices.',
+      demoLink: 'https://atesta-solutions.web.app/',
+    },
+    {
+      imgSrc: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/Wanderlust2.gif',
+      title: 'Travel Agency Landing page',
+      description: 'A stunning landing page for a travel agency crafted using React and Slick Carousel. Built with React for seamless UI, Slick Carousel for smooth image sliders,Tailwind CSS for sleek styling.',
+      demoLink: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/Wanderlust2.gif',
+    },
+    {
+      imgSrc: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/product.gif',
+      title: 'Product Slider',
+      description: 'A product slider built using React , Tailwind CSS , MongoDB ,Express.js. Features include, Responsive product cards with details, Sliding effect to showcase multiple products and Integration with a MongoDB backend for seamless data retrieval.',
+      demoLink: 'https://neyoportfolio.s3.eu-north-1.amazonaws.com/product.gif',
+    }
+  ];
