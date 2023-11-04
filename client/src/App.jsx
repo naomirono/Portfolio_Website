230c69d92a01a0774bd2ab2be05846675c989890
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-04 20:26:38 +0300

    Delete initial imports

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,5 @@
 import React from 'react';
 import Header from './components/Header';
-import Home from './components/Home';
-import About from './components/About';
-import Skills from './components/Skills';
-import Qualification from './components/Qualification';
-import Services from './components/Service';
-import Portfolio from './components/Portfolio';
-import Project from './components/Project';
 import ContactPage from './components/ContactPage';
 import LandingPage from './components/LandingPage';
 import { BrowserRouter as Router,Routes, Route} from 'react-router-dom';
