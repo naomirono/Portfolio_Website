374729aaf18575c7a2db80a99c4f38f5d66731b9
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-05 13:04:04 +0300

    Import skills js file

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,29 +1,9 @@
 import React, { useState } from 'react';
-import { FiCode, FiServer } from 'react-icons/fi';
+import { skillsData } from '../mock/skills';
 
 function Skills() {
   const [openSkill, setOpenSkill] = useState(0);
 
-  const skillsData = [
-    {
-      icon: <i className="uil uil-brackets-curly skills_icon"></i>,
-      title: 'Frontend Developer',
-      subtitle: 'More than 1 year',
-      items: ['HTML5', 'CSS3', 'TailwindCss', 'Javascript', 'React']
-    },
-    {
-      icon: <i className="uil uil-server-network skills_icon"></i>,
-      title: 'Backend Developer',
-      subtitle: 'More than 1 year',
-      items: ['Node Js', 'MongoDB', 'Firebase', 'Express']
-    },
-    {
-      icon: <FiServer className="skills_icon" />,
-      title: 'Version Control Tools',
-      subtitle: 'More than 1 year',
-      items: ['Git', 'Github']
-    }
-  ];
 
   const handleSkillClick = (index) => {
     if (openSkill === index) {
