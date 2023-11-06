0e43ec8f6943c6563fa11f3f9db603847b84bfbc
Naomi Cherotich <naomirono1454@gmail.com>
2023-11-06 12:59:44 +0300

    Delete the skills js

--- client/src/components/Skills.jsx
+++ client/src/components/Skills.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { skillsData } from '../mock/skills';
+import { FiServer } from 'react-icons/fi';
 
 function Skills() {
   const [openSkill, setOpenSkill] = useState(0);
@@ -16,6 +16,27 @@ function Skills() {
   const handleMouseLeave = () => {
     setOpenSkill(null); 
   };
+
+  const skillsData = [
+    {
+      icon: <i className="uil uil-brackets-curly skills_icon"></i>,
+      title: 'Frontend Developer',
+      subtitle: 'More than 1 year',
+      items: ['HTML5', 'CSS3', 'TailwindCss', 'Javascript', 'React']
+    },
+    {
+      icon: <i className="uil uil-server-network skills_icon"></i>,
+      title: 'Backend Developer',
+      subtitle: 'More than 1 year',
+      items: ['Node Js', 'MongoDB', 'Firebase', 'Express']
+    },
+    {
+      icon: <FiServer className="skills_icon" />,
+      title: 'Version Control Tools',
+      subtitle: 'More than 1 year',
+      items: ['Git', 'Github']
+    }
+  ];
 
   return (
     <section className="skills section" id="skills">
